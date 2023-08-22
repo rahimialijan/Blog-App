73559b3d3c7b955c3ce97352989643f4f21140bf
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-08-23 01:27:00 +0500

    Modify the database.yml file and Create_posts.rb file

--- db/migrate/20230822183655_create_posts.rb
+++ db/migrate/20230822183655_create_posts.rb
@@ -4,8 +4,6 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.references :author_id, null: false, foreign_key: { to_table: :users }
       t.string :title
       t.text :text
-      #t.datetime :created_at, default: -> { 'CURRENT_TIMESTAMP' }
-      #t.datetime :updated_at, default: -> { 'CURRENT_TIMESTAMP' }, on_update: -> { 'CURRENT_TIMESTAMP' }
       t.integer :comments_counter
       t.integer :likes_counter
 
