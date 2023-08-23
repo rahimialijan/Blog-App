cf34f91b935c778afb92e69c161baa31ff4a737e
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-08-23 23:44:08 +0500

    update the comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  after_create :update_comment_counter
+
   def update_comment_counter
     posts.update(comments_counter: post.comments.count)
   end
