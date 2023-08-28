27221a0d3d57f5da0707120a256584397621fbda
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-08-28 22:34:33 +0500

    Add controllers, routes and view files

---
+++ app/views/posts/index.html.erb
@@ -0,0 +1,1 @@
+<h1>All posts</h1>
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # config/routes.rb
+
+  # All users page
+  get '/users', to: 'users#index', as: 'users'
+
+  # Single user page
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  # All posts by a given user page
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+
+  # Single post page
+  get '/posts/:id', to: 'posts#show', as: 'post'
 
   # Defines the root path route ("/")
   # root "articles#index"
