7605396f1733f3946d8bc34b28539d83f3ec6338
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-08-29 10:15:02 +0500

    Fix the post issue according the reviewer suggestion

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,9 @@ Rails.application.routes.draw do
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # config/routes.rb
-
-  # All users page
-  get '/users', to: 'users#index', as: 'users'
-
-  # Single user page
-  get '/users/:id', to: 'users#show', as: 'user'
-
-  # All posts by a given user page
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-
-  # Single post page
-  get '/posts/:id', to: 'posts#show', as: 'post'
-
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
