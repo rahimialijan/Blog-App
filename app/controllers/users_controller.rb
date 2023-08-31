854c1f78ea7a39dc9f2a0bfc960c8917d28bcec2
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-08-31 11:15:38 +0500

    make changes on styles and and view files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @recent_posts = @user.recent_posts
+    @users = User.find(params[:id])
+    @recent_posts = @users.recent_posts
   end
 end
