a9238d5f4df0aa6561a6acd66d0dc593fad07e82
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-09-01 11:24:55 +0500

    fix the linter errors

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.2.2'
 
-gem 'rake', '~> 13.0'
 gem 'pg'
+gem 'rake', '~> 13.0'
 
 gem 'rails-controller-testing'
 gem 'rubocop', '>= 1.0', '< 2.0'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,24 +2,24 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(created_at: :asc)
-    @post= Post.new
+    @post = Post.new
   end
 
-  def show 
+  def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
 
   def new
     @user = User.find(params[:user_id])
-    @post= Post.new
+    @post = Post.new
   end
 
   def create
     set_current_user(params[:user_id])
-    @post =@current_user.posts.build(post_params)
-    @post.comments_counter=0
-    @post.likes_counter=0
+    @post = @current_user.posts.build(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
 
     if @post.save
       redirect_to user_post_path(@current_user.id, @post.id), notice: 'Post was successfully Created'
@@ -27,7 +27,9 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
   private
+
   def post_params
     params.require(:post).permit(:title, :text)
   end
