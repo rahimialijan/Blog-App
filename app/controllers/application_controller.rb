f28d3b19b26090f6340e3ac82a06f02e9b78ddc9
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-09-05 23:40:31 +0500

    fix the sign-up page issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, :sign_up_allawed_parameters, if: :devise_controller?
 
+  protected
+
+  def sign_up_allawed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :posts_counter)
+    end
+  end
 end
